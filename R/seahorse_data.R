19c4e674d56396aada28e2cb2936869ef4f9ca3a
ramirez-a <akram@bu.edu>
2017-03-09 16:17:06 -0500

    Seahorse data.  Need to double check description file

--- R/seahorse_data.R
+++ R/seahorse_data.R
@@ -1,5 +1,6 @@
 #' @title Seahorse XF Analyzer data on brown and white adipocytes
-#' @description Measures of OCR and PPR during the oxdiative stress test on brown and white culture adipocytes.
+#' @description Measures of OCR and PPR during the oxdiative stress test on brown and white culture adipocytes. 
+#' There are five regions: basal, pyruvate, oligomycin, fccp, and rotenone.
 #' @docType data
 #' @usage data(seahorse_data)
 #' @format A data.frame
