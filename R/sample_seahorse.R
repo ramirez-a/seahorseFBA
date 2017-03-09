76be13abe43686fdbe8e5dd6b43e80a7e23ec6b9
ramirez-a <akram@bu.edu>
2017-03-09 16:15:35 -0500

    First functional form.  Works as expected.

--- R/sample_seahorse.R
+++ R/sample_seahorse.R
@@ -5,24 +5,28 @@
 #'A data.frame with sampled measurements for the specified sample
 #'@param x A data.frame
 
-sample_seahorse <- function(x){
-  bsamples <- length(unique(x$Sample))
-  output_mat <- cbind(x, matrix(0, ncol=2, nrow=bsamples))
-  colnames(output_mat)[(ncol(output_mat)-1):ncol(output_mat)] <- c("ATP_flux", "ATP_leak")
+sample_seahorse <- function(x, nsamples=150, sample=NULL){
+  if(is.null(sample)){
+    stop("Please specify sample. The sample must exist in the rownames of x")
+  }
   
-  for(i in 1:bsamples){
-    output_mat[i,"OCR_basal"] <- rnorm(1,mean=x[i,"OCR_basal"],sd=x[i,"OCR_basal_sd"])
-    output_mat[i,"OCR_oligo"] <- rnorm(1,mean=x[i,"OCR_oligo"],sd=x[i,"OCR_oligo_sd"])
-    output_mat[i,"OCR_fccp"] <- rnorm(1,mean=x[i,"OCR_fccp"],sd=x[i,"OCR_fccp_sd"])
-    output_mat[i,"OCR_rotenone"] <- rnorm(1,mean=x[i,"OCR_rotenone"],sd=x[i,"OCR_rotenone_sd"])
-    
-    output_mat[i,"PPR_basal"] <- rnorm(1,mean=x[i,"PPR_basal"],sd=x[i,"PPR_basal_sd"])
-    output_mat[i,"PPR_oligo"] <- rnorm(1,mean=x[i,"PPR_oligo"],sd=x[i,"PPR_oligo_sd"])
-    output_mat[i,"PPR_fccp"] <- rnorm(1,mean=x[i,"PPR_fccp"],sd=x[i,"PPR_fccp_sd"])
-    output_mat[i,"PPR_rotenone"] <- rnorm(1,mean=o[i,"PPR_rotenone"],sd=x[i,"PPR_rotenone_sd"])
-    
-    output_mat[i,"ATP_flux"] <- 4.6*(output_mat[i,"OCR_basal"] - output_mat[i,"OCR_oligo"])
-    output_mat[i,"ATP_leak"] <- 4.6*(output_mat[i,"OCR_oligo"] - output_mat[i,"OCR_rotenone"])
-  }  
+  output_mat <- matrix(0, ncol=nsamples, nrow=8)
+  colnames(output_mat) <- paste0("sample", 1:nsamples)
+  rownames(output_mat) <- c("OCR_basal", "OCR_oligo", "OCR_fccp", "OCR_rotenone", "PPR_basal", "PPR_oligo", "PPR_fccp", "PPR_rotenone")
+  
+  i <- sample
+  output_mat["OCR_basal",] <- rnorm(nsamples,mean=x[i,"OCR_basal"],sd=x[i,"OCR_sd_basal"])
+  output_mat["OCR_oligo",] <- rnorm(nsamples,mean=x[i,"OCR_oligo"],sd=x[i,"OCR_sd_oligo"])
+  output_mat["OCR_fccp",] <- rnorm(nsamples,mean=x[i,"OCR_fccp"],sd=x[i,"OCR_sd_fccp"])
+  output_mat["OCR_rotenone",] <- rnorm(nsamples,mean=x[i,"OCR_rotenone"],sd=x[i,"OCR_sd_rotenone"])
+  
+  output_mat["PPR_basal",] <- rnorm(nsamples,mean=x[i,"PPR_basal"],sd=x[i,"PPR_sd_basal"])
+  output_mat["PPR_oligo",] <- rnorm(nsamples,mean=x[i,"PPR_oligo"],sd=x[i,"PPR_sd_oligo"])
+  output_mat["PPR_fccp",] <- rnorm(nsamples,mean=x[i,"PPR_fccp"],sd=x[i,"PPR_sd_fccp"])
+  output_mat["PPR_rotenone",] <- rnorm(nsamples,mean=x[i,"PPR_rotenone"],sd=x[i,"PPR_sd_rotenone"])
+  
+#   output_mat[i,"ATP_flux"] <- 4.6*(output_mat[i,"OCR_basal"] - output_mat[i,"OCR_oligo"])
+#   output_mat[i,"ATP_leak"] <- 4.6*(output_mat[i,"OCR_oligo"] - output_mat[i,"OCR_rotenone"])
+
   output_mat
 }
